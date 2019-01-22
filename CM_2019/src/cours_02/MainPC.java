96ae3373d2a7f43542c8d37f1d25e0026c1bfbed
MoeSyszlak <karimsassi01@gmail.com>
2019-01-22 22:07:23 +0100

    V2

--- CM_2019/src/cours_02/MainPC.java
+++ CM_2019/src/cours_02/MainPC.java
@@ -1,10 +1,35 @@
 package cours_02;
 
 public class MainPC {
+	private Computer computer;
+	private Cpu cpu;
+	private Manufacturer manufacturer;
+	
+	
+	public MainPC() {
+
+	}
+
+
+	public MainPC(Computer computer, Cpu cpu, Manufacturer manufacturer) {
+
+		this.computer = computer;
+		this.cpu = cpu;
+		this.manufacturer = manufacturer;
+	}
+
+
+	@Override
+	public String toString() {
+		return "MainPC [computer=" + computer + ", cpu=" + cpu + ", manufacturer=" + manufacturer + "]";
+	}
+
 
 	public static void main(String[] args) {
 		
-
+		
+		
+		
 	}
 
 }
