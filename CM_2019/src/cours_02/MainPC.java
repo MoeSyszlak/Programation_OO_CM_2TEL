ebff6fc4ee22b5ee09523cc1994f1138fe7123ac
MoeSyszlak <karimsassi01@gmail.com>
2019-01-26 10:16:44 +0100

    Version finale

--- CM_2019/src/cours_02/MainPC.java
+++ CM_2019/src/cours_02/MainPC.java
@@ -1,35 +1,25 @@
 package cours_02;
 
+
 public class MainPC {
-	private Computer computer;
-	private Cpu cpu;
-	private Manufacturer manufacturer;
-	
-	
-	public MainPC() {
 
-	}
-
-
-	public MainPC(Computer computer, Cpu cpu, Manufacturer manufacturer) {
-
-		this.computer = computer;
-		this.cpu = cpu;
-		this.manufacturer = manufacturer;
-	}
-
-
-	@Override
-	public String toString() {
-		return "MainPC [computer=" + computer + ", cpu=" + cpu + ", manufacturer=" + manufacturer + "]";
-	}
 
 
 	public static void main(String[] args) {
+		int i;
+		Computer tabPC[] =new Computer[3];
+		tabPC[0] = new Computer(new Cpu(new Manufacturer("Intel"), "Core I3"),new Manufacturer ("Dell"), 485.23);
+		tabPC[1] = new Computer(new Cpu(new Manufacturer("Intel"), "Core I5	"),new Manufacturer ("Microsoft"), 1501.92);
+		tabPC[2] = new Computer(new Cpu(new Manufacturer("Intel"), "Celeron N3160"),new Manufacturer ("Medion"), 269.99);
 		
+		for(i=0;i<3;i++) {
+		System.out.println(tabPC[i]);
+		}
 		
-		
-		
+	
+
+
+
 	}
 
 }
